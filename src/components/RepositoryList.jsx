df3b415027e8c7471213cd761a804a1a9b4d61a1
Amanda <amandaduuaarte@gmail.com>
2021-12-05 14:44:08 -0300

    Adicionando os repositorios dinamicamente

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,19 +1,23 @@
+import {useState, useEffect} from 'react';
 import { RepositoryItem } from "./RepositoryItem";
-
-const repository ={
-  name:'unform',
-  description:'Repositório em react',
-  link:'http://unform/unform'
-}
+import "../styles/repositories.scss";
+  
 export function RepositoryList (){
+  const [repositories,setRepositories] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(res =>res.json())
+    .then(data => setRepositories(data))
+  }, [repositories]) 
+  //função que quero execultar quando algo mudar no caso é a var repositories
   return (
     <section className="repository-list">
       <h1>Lista de repositórios: </h1> 
       <ul>
-       <RepositoryItem repository={repository}/>
-       <RepositoryItem repository={repository}/>
-       <RepositoryItem repository={repository} />
-       <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return  <RepositoryItem  key={repository.name} repository={repository}/>
+        })}
       </ul>
     </section>
   );
